2a3bcde58369aacc8c358016da04ca4a149e25fb
Predrag Markovljev <predrag.markovljev@gmail.com>
2023-01-07 16:01:02 +0100

    Created TheNavbar.vue and its template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import TheNavbar from "@/components/TheNavbar.vue";
 </script>
 
 <template>
+  <the-navbar />
   <RouterView />
 </template>
 
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="bg-weather-primary sticky top-0 shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center text-white py-6"
+    >
+      <!-- Logo & App name -->
+      <router-link :to="{ name: 'home' }">
+        <div class="flex gap-3">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">The Local Weather</p>
+        </div>
+      </router-link>
+
+      <!-- info & save city -->
+      <div class="flex justify-end flex-1 gap-3">
+        <i
+          class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+        ></i>
+        <i
+          class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+        ></i>
+      </div>
+
+      <!-- modal window -->
+      <div></div>
+    </nav>
+  </header>
+</template>
